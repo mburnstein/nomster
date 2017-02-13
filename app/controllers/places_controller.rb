fce8be38f20ead5ef94be1fb82c762c7271a68bb
Michael Burnstein <michael@burnstein.com>
2017-02-13 07:38:08 +0000

    Validation errors

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,13 @@ class PlacesController < ApplicationController
     end
 
     def create
-      current_user.places.create(place_params)
-      redirect_to root_path
+      @place = current_user.places.create(place_params)
+      if @place.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+        redirect_to root_path
+      end
     end
 
     def show
@@ -32,7 +37,11 @@ class PlacesController < ApplicationController
         return render text: 'Bad Hacker!', status: :forbidden
       end
       @place.update_attributes(place_params)
-      redirect_to root_path
+      if @place.valid?
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
 
     def destroy
