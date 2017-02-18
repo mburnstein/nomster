a1d316fb71ba444aadb6de860ef7c3a9ca84196e
Michael Burnstein <michael@burnstein.com>
2017-02-18 18:46:33 +0000

    Comments model set up

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
