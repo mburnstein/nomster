76bc514506e22f826d06894beb42256eec787eed
Michael Burnstein <michael@burnstein.com>
2017-03-16 23:56:17 +0000

    Added column for photos but still not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316042911) do
+ActiveRecord::Schema.define(version: 20170316235119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170316042911) do
     t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json     "pictures"
     t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
   end
 
