5300791da059ba69e31942b986b04fb10603a14c
Michael Burnstein <michael@burnstein.com>
2017-03-19 20:40:04 +0000

    email still not working

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,24 +1,10 @@
-class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "michael@burnstein.com",
-         subject: "A comment has been added to your place")
+class NotificationMailer < ApplicationMailer
+  default from: 'no_reply@nomstersf.com'
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}")
   end
 end
-
-
-
-
-
-
-
-#class NotificationMailer < ApplicationMailer
-  #default from: 'no_reply@nomstersf.com'
-  #def comment_added(comment)
-    #@place = comment.place
-    #@place_owner = @place.user
-    #mail(to: @place_owner.email,
-      #subject: "A comment has been added to your place")
-  #end
-#
